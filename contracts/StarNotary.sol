883291161534072b8c32ed77d1f67f69d96944d0
Pitipon Pluemworasawat <pitipon@gmail.com>
2022-07-29 22:09:46 +0800

    add name and symbol

--- contracts/StarNotary.sol
+++ contracts/StarNotary.sol
@@ -14,7 +14,8 @@ contract StarNotary is ERC721 {
     // Implement Task 1 Add a name and symbol properties
     // name: Is a short name to your token
     // symbol: Is a short string like 'USD' -> 'American Dollar'
-    
+    string public name = "MO Star Token";
+    string public symbol = "MST";
 
     // mapping the Star with the Owner Address
     mapping(uint256 => Star) public tokenIdToStarInfo;
